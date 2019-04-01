89b2d5c084c72b355d8d6852c97c698993412fb8
Ressort Aloïs <ressort.alois@gmail.com>
2019-04-01 14:57:32 +0200

    WebGL works

--- resources/views/amphi.blade.php
+++ resources/views/amphi.blade.php
@@ -1,1 +1,31 @@
-echo azaaz
+<!DOCTYPE html>
+<html>
+<head>
+
+    <meta charset=utf-8>
+    <title>My first three.js app</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+</head>
+<body>
+
+<div id="webgl"></div>
+
+<script src="{{asset('js/dat.gui.min.js')}}"></script>
+
+<script src="{{asset('js/three.js')}}"></script>
+<!--<script src="http://mrdoob.github.com/three.js/build/three.min.js"></script>-->
+
+<script src="{{asset('js/OrbitControls.js')}}"></script>
+<script src="{{asset('js/MapControls.js')}}"></script>
+<script src="{{asset('js/MTLLoader.js')}}"></script>
+<script src="{{asset('js/OBJLoader.js')}}"></script>
+<!--<script src="scripts/OBJMTLLoader.js"></script>-->
+<script src="{{asset('js/bleu.js')}}"></script>
+{{--<script src="{{asset('js/rouge.js')}}"></script>--}}
+{{--<script src="{{asset('js/jaune.js')}}"></script>--}}
+{{--<script src="{{asset('js/vert.js')}}"></script>--}}
+{{--<script src="{{asset('js/violet.js')}}"></script>--}}
+
+
+</body>
